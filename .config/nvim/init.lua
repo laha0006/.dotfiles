980cbf4dd6a00e221117c0a9f563c0bc5fd7f6e3
Lars <144362461+laha0006@users.noreply.github.com>
2025-03-29 20:49:02 +0100

    added oil.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -244,6 +244,21 @@ require('lazy').setup({
 --  'tpope/vim-sleuth',
   'ThePrimeagen/vim-be-good',
   {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      require('oil').setup()
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+  {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
